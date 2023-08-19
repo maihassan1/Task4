b8a4ed465e6dab39cc288d31f040c4f86fd1191b
maihassan1 <114602980+maihassan1@users.noreply.github.com>
2023-08-19 23:35:01 +0300

    final commit

--- lib/home.dart
+++ lib/home.dart
@@ -4,104 +4,40 @@ class  Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white,
-      body:  SafeArea(
-        child: Container(margin: EdgeInsetsDirectional.only(start:30,top:20,end:20),
-          child: Column( children :[
-            Container(
-                child:
-              Text("Menu                                                                 ",style: TextStyle(color: Colors.black,fontSize: 40 ,fontWeight: FontWeight.w900))
-            ),SizedBox(height: 15),
-            Row(
-              children: [
-                Container(
-                    child:
-                  Text("HOT DRINKES",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.red,fontSize:30 ),)
-                ),
-              Container(
-                child: Text("  Partition",style: TextStyle(color: Colors.black,fontSize: 30)),
+    return Scaffold(
+        appBar: AppBar(title: Text("Task 1") ,centerTitle: true,
+          leading:Icon(Icons.menu) , backgroundColor:Colors.greenAccent ,
+        ),
+        body:  SafeArea(child:
+        Row (
+            children:
+            [ Container(margin: EdgeInsetsDirectional.only(
+                start:30, top :50
+            ),
+              child: Column(
+                  children :[
+                    Container(
+                      height: 200,width: 150,color: Colors.red, ),
+                    SizedBox(height: 175),
+                    Container(
+                      height: 200,width: 150,color: Colors.red, )
+                  ]),
+            )
+              ,
+              Container(margin:EdgeInsetsDirectional.only(
+                  start:60, top :50) ,
+                child: Column(
+                    children :[
+                      Container(
+                        height: 200,width: 150,color: Colors.yellow, ),
+                      SizedBox(height: 175),
+                      Container(
+                        height: 200,width: 150,color: Colors.deepOrangeAccent, )
+                    ]),
               )
-              ],
-            ),SizedBox(height:15),
-                  Container(
-                    height: 140,width:450,decoration: BoxDecoration( color: Colors.white,borderRadius: BorderRadius.circular(10),border: Border.all(color: Colors.black,width: 2) )
-                  ,
-                    child: Row(
-                    children: [
-                      Container(margin: EdgeInsetsDirectional.only(start:10,top:10),
-                        child: Column(children: [
-                              Image.asset("c.png")]),
-                      ),
-                      SizedBox(width: 170),
-                      Container(margin: EdgeInsetsDirectional.only(top:20),
-                        child: Column(
-                          children: [
-                            Text("CAPPUCCINO",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                            SizedBox(height: 60,),
-                            Row(
-                              children: [
-                                Text("40 L.E",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),SizedBox(width: 60,),
-                              Image.asset("n.jpg")],
-                            )]
-                          ,
-                        ),
-                      )
-]),),
-            SizedBox(height:15),
-            Container(
-              height: 140,width:450,decoration: BoxDecoration( color: Colors.white,borderRadius: BorderRadius.circular(10),border: Border.all(color: Colors.black,width: 2) )
-              ,
-              child: Row(
-                  children: [
-                    Container(margin: EdgeInsetsDirectional.only(start:10,top:30),
-                      child: Column(children: [
-                        Image.asset("download.jpg")]),
-                    ),
-                    SizedBox(width: 120),
-                    Container(margin: EdgeInsetsDirectional.only(top:20),
-                      child: Column(
-                        children: [
-                          Text("COFFEE",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                          SizedBox(height: 60,),
-                          Row(
-                            children: [
-                              Text("30 L.E",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),SizedBox(width: 60,),
-                              Image.asset("n.jpg")],
-                          )]
-                        ,
-                      ),
-                    )
-                  ]),)
+            ])
 
-            ,SizedBox(height:15),
-            Container(
-              height: 140,width:450,decoration: BoxDecoration( color: Colors.white,borderRadius: BorderRadius.circular(10),border: Border.all(color: Colors.black,width: 2) )
-              ,
-              child: Row(
-                  children: [
-                    Container(margin: EdgeInsetsDirectional.only(start:10,top:10),
-                      child: Column(children: [
-                        Image.asset("1.jpg")]),
-                    ),
-                    SizedBox(width: 120),
-                    Container(margin: EdgeInsetsDirectional.only(top:20),
-                      child: Column(
-                        children: [
-                          Text("MOCHA",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                          SizedBox(height: 60,),
-                          Row(
-                            children: [
-                              Text("40 L.E",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),SizedBox(width: 60,),
-                              Image.asset("n.jpg")],
-                          )]
-                        ,
-                      ),
-                    )
-                  ]),) ],
-                  ),)
+        ) ) ;
 
-          ),
-        );
-
-    }
+  }
 }
