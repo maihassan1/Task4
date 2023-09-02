feac6d6f80dace911c6d820b92168a4ddc0b6504
maihassan1 <114602980+maihassan1@users.noreply.github.com>
2023-09-02 23:25:35 +0300

    final commit3

--- lib/home.dart
+++ lib/home.dart
@@ -1,146 +1,328 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'Screens/secondScreen.dart';
-class  Home extends StatefulWidget {
-  const Home ({super.key});
-  static var id = "sds";
+import 'Screens/Screens/Second.dart';
+import 'Widgets/Question.dart';
+class Home extends StatefulWidget {
+  const Home({super.key});
+
   @override
   State<Home> createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
-  bool obscure = true ;
-  bool obscure2 = true;
-  String? selected;
-  List<String> Gender = ['Male' , 'Female'];
-  String? selected2 ;
-  List<String>University =['AUC' , 'CU', 'BUE','ACU'];
-  String? selected3;
-  List<String>Grade = ['G1','G2','G3','G3'];
+  final _fromKeyScreen1 = GlobalKey<FormState>();
   @override
+  int? selected,selected2,selected3,selected4,selected5,res=0;
   Widget build(BuildContext context) {
     return Scaffold(
-      body : SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
+      body :SingleChildScrollView(
+        child: Form( key : _fromKeyScreen1,
           child: Column(
             children: [
-              AppBar(title: Text("Register")),
-              TextFormField(
-                decoration: InputDecoration(hintText: "E-mail",
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10) )),
-              ),
-              SizedBox(height: 7,),
-              TextFormField(decoration: InputDecoration(suffixIcon:
-              IconButton(
-                onPressed: (){
-                  setState(() {
-                    obscure =! obscure;
-                  });
-                },
-                icon: Icon(Icons.remove_red_eye),
-              ),
-                  hintText: "Password",
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10) )),
-                keyboardType: TextInputType.emailAddress,
-                obscureText: obscure,
-              ),
-              SizedBox(height: 7,),
-              TextFormField(decoration: InputDecoration(suffixIcon:
-        IconButton(
-          onPressed: (){
-            setState(() {
-              obscure2 =! obscure2;
-            });
-          },
-          icon: Icon(Icons.remove_red_eye),
-        ),
-            hintText: "Password",
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10) )),
-          keyboardType: TextInputType.emailAddress,
-                obscureText: obscure2,
-        ),
-              SizedBox(height: 7,),
-              TextFormField(decoration: InputDecoration(hintText: "Phone",
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(10) )),
-                  keyboardType: TextInputType.number
-              ),SizedBox(height:50),
-              Row(mainAxisAlignment:MainAxisAlignment.center ,
-                children: [
-                  Text("Gender",
-                      style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
-                  SizedBox(width:140,),
-                  Text("University",
-                      style: TextStyle(fontSize: 25,
-                          fontWeight: FontWeight.bold))
-                ],
-              ),
-              Row(
-                children: [
-                  Container(decoration: BoxDecoration(border: Border.all(color: Colors.yellow
-                      ,width:2 ),
-                      borderRadius:BorderRadius.circular(15)),
-                    child: DropdownButton<String>( padding: EdgeInsetsDirectional.symmetric(horizontal:70),
-                      items: Gender.map((e) =>DropdownMenuItem(child: Text(e),value: e,)).toList(), onChanged: (value){
-                      setState(() {
-                        selected = value as String;
-                      });
-                    },value: selected,),
-                  ),SizedBox(width:60 ,),
-                  Container(decoration: BoxDecoration(border: Border.all(color: Colors.yellow,
-                      width:2 ),
-                      borderRadius:BorderRadius.circular(15)),
-                    child: DropdownButton<String>( padding: EdgeInsetsDirectional.symmetric(horizontal:50 ),
-                      items: University.map((e) =>DropdownMenuItem(child: Text(e),value: e,)).toList(), onChanged: (value){
-                      setState(() {
-                        selected2 = value as String;
-                      });
-                    },value: selected2,),
-                  )
-                ],
-              ), SizedBox(height: 30),
-              Text("Grade",
-                  style: TextStyle(fontSize: 25,
-                      fontWeight: FontWeight.bold)),
-              Container(decoration: BoxDecoration(border: Border.all(color: Colors.yellow,
-                  width:2 )
-                  ,borderRadius:BorderRadius.circular(15) ),
-                child: DropdownButton<String>(padding: EdgeInsetsDirectional.symmetric(horizontal:50 ),
-                  items: Grade.map((e) =>DropdownMenuItem(child: Text(e),value: e,)).toList(), onChanged: (value){
-                    setState(() {
-                      selected3 = value as String;
-                    });
-                  },value: selected3,),
-              ),SizedBox(height:50),
-              ElevatedButton(onPressed: (){}
-                  ,style: ElevatedButton.styleFrom(primary: Colors.yellow,
-                  fixedSize: Size(500, 50),
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))),
-                  child: Text("Register",style: TextStyle(fontSize: 30,
-                      color: Colors.white),))
-              , SizedBox(height: 15),
-              Text("OR",style: TextStyle(fontSize: 30))
-              , SizedBox(height: 15),
-              ElevatedButton(onPressed: (){
-                Navigator.of(context).pushNamed(secondScreen.id);
-              }
-                  , child: Text("Have an account ? Log in",style: TextStyle(fontSize: 15,
-                      color: Colors.black),))
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 30),
+                child: Container(padding: EdgeInsets.symmetric(vertical: 20),
+                  height: 300,width :450,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)
+                      ,color: Colors.black12),
+                  child: Column(
+                    children: [
+                      Text("Q1. What is Flutter?",
+                          style: TextStyle(fontWeight: FontWeight.bold,
+                              fontSize: 25
+                          )),
 
+                      RadioListTile(value: 0,
+                        groupValue: selected,
+                        onChanged: (value)
+                        {
+                          setState(() {
+                            selected= value ;
+                          });
+                        },title: Text("A programming language"),
+                      ),
+                      RadioListTile(value: 1,
+                          groupValue: selected,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected= value ;
+                            });
+                          },
+                          title: Text("A mobile app development framework")),
+                      RadioListTile(value: 2,
+                          groupValue: selected,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected= value ;
+                            });
+                          },title: Text("A database management system")
+                      ),
+                      RadioListTile(value: 3,
+                          groupValue: selected,
+                          onChanged: (value)
+                          {  if(value == 1){
+                            res = res! + 2 ;
+                          }
+                            setState(() {
+                              selected= value ;
+                              }
+                            );
+                          },title: Text("An operating system"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 30),
+                child: Container(padding: EdgeInsets.symmetric(vertical: 20),
+                  height: 300,width :450,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)
+                      ,color: Colors.black12),
+                  child: Column(
+                    children: [
+                      Text("Q2. What is Flutter?",
+                          style: TextStyle(fontWeight: FontWeight.bold,
+                              fontSize: 25
+                          )),
 
+                      RadioListTile(value: 0,
+                        groupValue: selected2,
+                        onChanged: (value)
+                        {
+                          setState(() {
+                            selected2= value ;
+                          });
+                        },title: Text("A programming language"),
+                      ),
+                      RadioListTile(value: 1,
+                          groupValue: selected2,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected2= value ;
+                            });
+                          },
+                          title: Text("A mobile app development framework")),
+                      RadioListTile(value: 2,
+                          groupValue: selected2,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected2= value ;
+                            });
+                          },title: Text("A database management system")
+                      ),
+                      RadioListTile(value: 3,
+                          groupValue: selected2,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected2= value ;
+                            });
+                          },
+                          title: Text("An operating system"))
 
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 30),
+                child: Container(padding: EdgeInsets.symmetric(vertical: 20),
+                  height: 300,width :450,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)
+                      ,color: Colors.black12),
+                  child: Column(
+                    children: [
+                      Text("Q3. What is Flutter?",
+                          style: TextStyle(fontWeight: FontWeight.bold,
+                              fontSize: 25
+                          )),
+
+                      RadioListTile(value: 0,
+                        groupValue: selected3,
+                        onChanged: (value)
+                        {
+                          setState(() {
+                            selected3 = value ;
+                          });
+                        },title: Text("A programming language"),
+                      ),
+                      RadioListTile(value: 1,
+                          groupValue: selected3,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected3 = value ;
+                            });
+                          },
+                          title: Text("A mobile app development framework")),
+                      RadioListTile(value: 2,
+                          groupValue: selected3 ,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected3 = value ;
+                            });
+                          },title: Text("A database management system")
+                      ),
+                      RadioListTile(value: 3,
+                          groupValue: selected3 ,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected3 = value ;
+                            });
+                          },
+                          title: Text("An operating system"))
+
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 30),
+                child: Container(padding: EdgeInsets.symmetric(vertical: 20),
+                  height: 300,width :450,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)
+                      ,color: Colors.black12),
+                  child: Column(
+                    children: [
+                      Text("Q4. What is Flutter?",
+                          style: TextStyle(fontWeight: FontWeight.bold,
+                              fontSize: 25
+                          )),
+
+                      RadioListTile(value: 0,
+                        groupValue: selected4,
+                        onChanged: (value)
+                        {
+                          setState(() {
+                            selected4 = value ;
+                          });
+                        },title: Text("A programming language"),
+                      ),
+                      RadioListTile(value: 1,
+                          groupValue: selected4,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected4 = value ;
+                            });
+                          },
+                          title: Text("A mobile app development framework")),
+                      RadioListTile(value: 2,
+                          groupValue: selected4,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected4 = value ;
+                            });
+                          },title: Text("A database management system")
+                      ),
+                      RadioListTile(value: 3,
+                          groupValue: selected4,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected4= value ;
+                            });
+                          },
+                          title: Text("An operating system"))
+
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 30),
+                child: Container(padding: EdgeInsets.symmetric(vertical: 20),
+                  height: 300,width :450,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(30)
+                      ,color: Colors.black12),
+                  child: Column(
+                    children: [
+                      Text("Q5. What is Flutter?",
+                          style: TextStyle(fontWeight: FontWeight.bold,
+                              fontSize: 25
+                          )),
+
+                      RadioListTile(value: 0,
+                        groupValue: selected5,
+                        onChanged: (value)
+                        {
+                          setState(() {
+                            selected5 = value ;
+                          });
+                        },title: Text("A programming language"),
+                      ),
+                      RadioListTile(value: 1,
+                          groupValue: selected5,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected5 = value ;
+                            });
+                          },
+                          title: Text("A mobile app development framework")),
+                      RadioListTile(value: 2,
+                          groupValue: selected5,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected5 = value ;
+                            });
+                          },title: Text("A database management system")
+                      ),
+                      RadioListTile(value: 3,
+                          groupValue: selected5,
+                          onChanged: (value)
+                          {
+                            setState(() {
+                              selected5 = value ;
+                            });
+                          },
+                          title: Text("An operating system"))
+
+                    ],
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.black,
+                  fixedSize: Size(200,70),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15))),
+                  onPressed: (){
+                    if(selected == 1)
+                      res = res! +1;
+                    if(selected2 == 1)
+                      res = res! + 1;
+                    if(selected3 == 1)
+                      res = res! + 1;
+                    if(selected4 == 1)
+                      res = res! +1;
+                    if(selected5 == 1)
+                      res = res! + 1;
+                    if(selected == null || selected2 == null ||selected3 == null || selected4 == null ||selected5 == null){
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text("Enter your answer")));
+                    }
+                    else
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content:Text("Your score is ${res.toString()}/5")));
+                  },
+                  child: Text("Submit",
+                style: TextStyle(fontSize: 25,
+                    fontWeight: FontWeight.bold
+                , color: Colors.white),
+                  ))
             ],
           ),
-        ),
-      )
+        )
+      ),
 
 
 
-
-
-
-    );
-
-
+   );
   }
 }
